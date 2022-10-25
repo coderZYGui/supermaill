61f89412de473b8e10ffaeab4f88cceee90d5a74
guizy <guichaoyang687@hellobike.com>
2022-10-25 14:05:14 +0800

    feat: 静态资源/tabbar引入

---
+++ src/components/common/tabbar/TabBarItem.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="tab-bar-item">
+    <div class="tab-bar-item" @click="itemClick">
+
+      <div v-if="!isActive">
+        <slot name="item-icon"></slot>
+      </div>
+
+      <!-- 图片active插槽 -->
+      <div v-else>
+        <slot name="item-icon-active"></slot>
+      </div>
+      <!-- 这样写样式不起效果  -->
+      <!-- <slot :class="{ active: isActive }" name="item-text"></slot> -->
+      <!-- 渲染的时候会直接把它这个替换成  <div slot="item-text">首页</div> -->
+      <!-- 一般不会直接在插槽上绑定动态属性，因为渲染时会把它替换掉
+       外面套一个div这样不会把原来的替换掉，上面两个最好也这样写  -->
+
+      <!--      <div :class="{ active: isActive }">-->
+      <div :style="activeStyle">
+        <slot name="item-text"></slot>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TabBarItem",
+  props: {
+    path: String,
+    activeColor: {
+      type: String,
+      default: "red"
+    }
+  },
+  data() {
+    return {
+      // isActive: true
+    }
+  },
+  computed: {
+    isActive() {
+      return this.$route.path.indexOf(this.path) !== -1
+    },
+    activeStyle() {
+      return this.isActive ? { color: this.activeColor } : {};
+    }
+  },
+  methods: {
+    itemClick() {
+      //console.log("itemClick")
+      this.$router.replace(this.path)
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+.tab-bar-item {
+  flex: 1; /* 等价flex-grow: 1 */
+  text-align: center;
+  height: 49px;
+  font-size: 15px;
+}
+
+.tab-bar-item img {
+  width: 24px;
+  height: 24px;
+  margin-top: 3px;
+  vertical-align: middle;
+}
+
+/*.active {*/
+/*  color: #c33979;*/
+/*}*/
+</style>
---
+++ src/components/content/mainTabbar/MainTabBar.vue
@@ -0,0 +1,56 @@
+<template>
+  <tab-bar>
+    <tab-bar-item path="/home" active-color="blue">
+      <!-- 具名插槽 -->
+
+      <!-- 具名插槽写法1 -->
+      <!--      <img slot="item-icon" src="../assets/img/tabbar/home.svg" alt="">-->
+
+      <!-- 具名插槽写法2 -->
+      <!--      <template v-slot:item-icon>-->
+      <!--        <img src="../assets/img/tabbar/home.svg" alt="">-->
+      <!--      </template>-->
+
+      <!-- 具名插槽写法3 -->
+      <template #item-icon>
+        <img src="@/assets/img/tabbar/home.svg" alt="">
+      </template>
+
+      <img slot="item-icon-active" src="@/assets/img/tabbar/home_active.svg" alt="">
+      <div slot="item-text">首页</div>
+    </tab-bar-item>
+    <tab-bar-item path="/category" active-color="skyblue">
+      <img slot="item-icon" src="@/assets/img/tabbar/category.svg" alt="">
+      <img slot="item-icon-active" src="@/assets/img/tabbar/category_active.svg" alt="">
+      <div slot="item-text">分类</div>
+    </tab-bar-item>
+    <tab-bar-item path="/cart">
+      <img slot="item-icon" src="@/assets/img/tabbar/shopcart.svg" alt="">
+      <img slot="item-icon-active" src="@/assets/img/tabbar/shopcart_active.svg" alt="">
+      <div slot="item-text">购物车</div>
+    </tab-bar-item>
+    <tab-bar-item path="/profile">
+      <img slot="item-icon" src="@/assets/img/tabbar/profile.svg" alt="">
+      <img slot="item-icon-active" src="@/assets/img/tabbar/profile_active.svg" alt="">
+      <div slot="item-text">我的</div>
+    </tab-bar-item>
+  </tab-bar>
+</template>
+
+<script>
+
+import tabBar from "@/components/common/tabbar/TabBar";
+import tabBarItem from "@/components/common/tabbar/TabBarItem";
+
+export default {
+  name: "MainTabBar",
+  components: {
+    tabBar,
+    tabBarItem
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
