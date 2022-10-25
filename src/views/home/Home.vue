33f0553a154460637331588f8dab09a2a8e0ca86
guizy <guichaoyang687@hellobike.com>
2022-10-25 22:44:34 +0800

    feat: 引入轮播图组件

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -5,17 +5,28 @@
         <div>购物街</div>
       </template>
     </nav-bar>
+
+    <swiper>
+      <swiper-item v-for="(item,index) in banners" :key="index">
+        <a :href="item.link">
+          <img :src="item.image" alt="">
+        </a>
+      </swiper-item>
+    </swiper>
   </div>
 </template>
 
 <script>
 import NavBar from "@/components/common/navbar/NavBar";
 import {getHomeMultidata} from "@/network/home";
+import {Swiper, SwiperItem} from "@/components/common/swiper";
 
 export default {
   name: "Home",
   components: {
-    NavBar
+    NavBar,
+    Swiper,
+    SwiperItem
   },
   data() {
     return {
