b77db01d189f902372de5942bc1380df11cd53ff
guizy <guichaoyang687@hellobike.com>
2022-11-01 11:05:54 +0800

    feat: 商品数据接口调整

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -60,7 +60,7 @@ import FeatureView from "@/views/home/childComps/FeatureView";
 import NavBar from "@/components/common/navbar/NavBar";
 import TabControl from "@/components/content/tabControl/TabControl";
 
-import {getHomeMultidata} from "@/network/home";
+import {getHomeMultidata, getHomeGoods} from "@/network/home";
 
 export default {
   name: "Home",
@@ -75,14 +75,35 @@ export default {
     return {
       banners: [],
       recommends: [],
-      titles: ['流行', '新款', '精品']
+      titles: ['流行', '新款', '精品'],
+      goods: {
+        'pop': {page: 0, list: []},
+        'new': {page: 0, list: []},
+        'sell': {page: 0, list: []}
+      }
     }
   },
   created() {
-    getHomeMultidata().then(res => {
-      this.banners = res.data.banner.list;
-      this.recommends = res.data.recommend.list;
-    })
+    this.getHomeMultidata()
+    this.getHomeGoods('pop')
+    this.getHomeGoods('new')
+    this.getHomeGoods('sell')
+  },
+  methods: {
+    getHomeMultidata() {
+      getHomeMultidata().then(res => {
+        this.banners = res.data.banner.list;
+        this.recommends = res.data.recommend.list;
+      })
+    },
+    // 请求商品数据
+    getHomeGoods(type) {
+      let page = this.goods[type].page + 1;
+      getHomeGoods(type, page).then(res => {
+        this.goods[type].list.push(...res.data.list)
+      })
+      this.goods[type].page += 1
+    }
   }
 }
 </script>
